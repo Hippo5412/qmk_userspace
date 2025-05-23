2cd4eff55e5cf0e96fb62558e961141806aba616
parker <parkerlane003@gmail.com>
2025-05-23 16:15:15 -0500

    fixed lalt on layers

--- keyboards/dactyl36/keymaps/default/keymap.c
+++ keyboards/dactyl36/keymaps/default/keymap.c
@@ -27,13 +27,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT(
         KC_F1,             KC_F2,            KC_F3,                 KC_F4,                   KC_F5,                                KC_F6,      KC_F7,              KC_F8,           KC_F9,            KC_F10,
-        LGUI_T(KC_F11),    LALT(KC_HOME),    LSFT_T(KC_PAGE_UP),    LCTL_T(KC_PAGE_DOWN),    KC_END,                               KC_LEFT,    RCTL_T(KC_DOWN),    RSFT_T(KC_UP),   RALT_T(KC_RGHT),  RGUI_T(KC_F12),
+        LGUI_T(KC_F11),    LALT_T(KC_HOME),    LSFT_T(KC_PAGE_UP),    LCTL_T(KC_PAGE_DOWN),    KC_END,                               KC_LEFT,    RCTL_T(KC_DOWN),    RSFT_T(KC_UP),   RALT_T(KC_RGHT),  RGUI_T(KC_F12),
         XXXXXXX,           XXXXXXX,          XXXXXXX,               XXXXXXX,                 XXXXXXX,                              XXXXXXX,    XXXXXXX,            XXXXXXX,         XXXXXXX,          XXXXXXX,
                                              XXXXXXX,               XXXXXXX,                 XXXXXXX,                              XXXXXXX,    XXXXXXX,            XXXXXXX
     ),
     [2] = LAYOUT(
         KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,
-        LGUI_T(KC_1),    LALT(KC_2),    LSFT_T(KC_3),    LCTL_T(KC_4),    KC_5,                               KC_6,    RCTL_T(KC_7),    RSFT_T(KC_8),   RALT_T(KC_9),  RGUI_T(KC_0),
+        LGUI_T(KC_1),    LALT_T(KC_2),    LSFT_T(KC_3),    LCTL_T(KC_4),    KC_5,                               KC_6,    RCTL_T(KC_7),    RSFT_T(KC_8),   RALT_T(KC_9),  RGUI_T(KC_0),
         KC_BSLS,    KC_SLSH,    KC_ASTR,    KC_MINS,    KC_EQL,     KC_GRV,     KC_QUOT,    XXXXXXX,    KC_LBRC,    KC_RBRC,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
